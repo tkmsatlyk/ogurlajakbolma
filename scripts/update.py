1028fbef406dc336ba6f9cea04c5ad2323aedd36
tkmsatlyk <nodewmx@gmail.com>
2026-09-08 00:24:17 +0500

    Remove 'happvpn' channel from CHANNELS list

--- scripts/update.py
+++ scripts/update.py
@@ -10,7 +10,6 @@ from datetime import datetime
 CHANNELS = [
     "https://t.me/s/ares_happ",
     "https://t.me/s/Richman_vpns",
-    "https://t.me/s/happvpn",
     "https://t.me/s/expensive_vpn",
     "https://t.me/s/aron58",
 ]
