13d9e29889c9711f85c8d656238da98583b50248
tkmsatlyk <nodewmx@gmail.com>
2026-09-16 10:41:33 +0500

    Update update.py

--- scripts/update.py
+++ scripts/update.py
@@ -17,7 +17,6 @@ CHANNELS = [
     "https://t.me/s/expensive_vpn",
     "https://t.me/s/aron58",
     "https://t.me/s/topserverss",
-    "https://t.me/s/happvpn",  
     "https://t.me/s/star_vpns",
     "https://t.me/s/v2speed_vpns",
 ]
